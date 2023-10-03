144ab1fe3e92c38546c1c499786a30f0e641c2cf
Simion Ombui <sombui@blomagris.co.ke>
2023-10-03 16:29:52 +0300

    first commit

---
+++ spatial_join.js
@@ -0,0 +1,10 @@
+var ecde = {
+"type": "FeatureCollection",
+"name": "spatial_join",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID_1": 2, "OBJECTID": 8876, "CODE": 13609.0, "SCHOOL_NAM": "TULIA", "LEVEL": "Primary", "Status": "Public", "County": "Kitui", "DISTRICT": "KITUI WEST", "ZONE": "TULIA", "SUB_COUNTY": "Kitui West", "Ward": "Mutonguni", "X_Coord": 37.97532, "Y_Coord": -1.1845, "Source": "Ministry of Education, 2016", "INFRASTRUC": "SEMI PERMANENT", "CAPACITY_O": 570, "Level_1": "ECDE", "Comments": "RUNNING and ACTIVE", "InputID": "TULIA", "TargetID": "MWANGYA", "Distance": 1784.64 }, "geometry": { "type": "Point", "coordinates": [ 37.974863143619245, -1.184143081038314 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 3, "OBJECTID": 8866, "CODE": 13595.0, "SCHOOL_NAM": "MWANGYA", "LEVEL": "Primary", "Status": "Public", "County": "Kitui", "DISTRICT": "KITUI WEST", "ZONE": "TULIA", "SUB_COUNTY": "Kitui West", "Ward": "Mutonguni", "X_Coord": 37.97781, "Y_Coord": -1.17062, "Source": "Ministry of Education, 2016", "INFRASTRUC": "PERMANENT", "CAPACITY_O": 339, "Level_1": "ECDE", "Comments": "RUNNING and ACTIVE", "InputID": "MWANGYA", "TargetID": "TULIA", "Distance": 1784.64 }, "geometry": { "type": "Point", "coordinates": [ 37.978685599560833, -1.173417900315826 ] } },
+{ "type": "Feature", "properties": { "OBJECTID_1": 4, "OBJECTID": 8839, "CODE": 13526.0, "SCHOOL_NAM": "KIAMANI", "LEVEL": "Primary", "Status": "Public", "County": "Kitui", "DISTRICT": "KITUI WEST", "ZONE": "TULIA", "SUB_COUNTY": "Kitui West", "Ward": "Mutonguni", "X_Coord": 37.96938, "Y_Coord": -1.16344, "Source": "Ministry of Education, 2016", "INFRASTRUC": "WOOD", "CAPACITY_O": 261, "Level_1": "ECDE", "Comments": "RUNNING and ACTIVE", "InputID": "KIAMANI", "TargetID": "MWANGYA", "Distance": 1931.42 }, "geometry": { "type": "Point", "coordinates": [ 37.970493586909583, -1.164591658777965 ] } }
+]
+}
